62cd5d8d76b5dd70d773678a007f934daebd6540
Victor Guerrero <Vguerrero42@gmail.com>
2021-02-23 14:59:23 -0500

    apollo provider and Nav container set up

--- client/App.js
+++ client/App.js
@@ -1,10 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import {ApolloClient,ApolloProvider,InMemoryCache} from '@apollo/client'
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+import {AppLoading} from 'expo'
+
 import {Home,LandingPage} from './components';
+
+const Stack = createStackNavigator()
+
+const cache = new InMemoryCache()
+
+
+const client = new ApolloClient({
+  uri: 'localhost:3000/graphql',
+  cache
+});
 
 export default function App() {
   return (
-    <LandingPage/>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Landing'>
+          <Stack.Screen
+            name='Landing'
+            component={LandingPage}
+          />
+          <Stack.Screen 
+            name='Home'
+            component={Home}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ApolloProvider>
   );
 }
 
